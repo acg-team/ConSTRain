a505d2c26a0f92f6a810f75892b01916ce42e189
maverbiest <mavh1995@gmail.com>
2024-06-21 16:16:44 +0200

    v0.2.0: increased modularity of `fn main`. Some style improvements

--- ConSTRain/src/bin/ConSTRain.rs
+++ ConSTRain/src/bin/ConSTRain.rs
@@ -1,11 +1,11 @@
-use anyhow::Result;
+use anyhow::{bail, Context, Result};
 use clap::Parser;
 use constrain::repeat::TandemRepeat;
 use constrain::utils::CopyNumberVariant;
 use rayon::{prelude::*, ThreadPoolBuilder};
 use rust_htslib::{
     bam::{self, HeaderView},
-    htslib,
+    htslib::{self, htsFile},
 };
 use std::collections::HashSet;
 use std::{error::Error, ffi, path::Path, sync::Arc};
@@ -104,6 +104,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .num_threads(cli.threads)
         .build_global()?;
     let chunksize = tr_regions.len() / cli.threads + 1;
+
     tr_regions.par_chunks_mut(chunksize).for_each(|tr_regions| {
         // Main work happens in this parallel iterator
         //
@@ -118,42 +119,24 @@ fn main() -> Result<(), Box<dyn Error>> {
             eprintln!("Launched thread {tidx}");
         }
 
-        let htsfile = rhtslib_from_path(&cli.alignment);
-        let header: *mut htslib::sam_hdr_t = unsafe { htslib::sam_hdr_read(htsfile) };
-        // we panic if we can't convert the alignment file name to a C string
-        let c_str = ffi::CString::new(cli.alignment.as_str()).expect("Internal 0 byte contained in alignment file name");
-        let idx: *mut htslib::hts_idx_t =
-            unsafe { htslib::sam_index_load(htsfile, c_str.as_ptr()) };
-        assert!(!idx.is_null(), "Unable to load index for alignment file!");
-
-        unsafe {
-            let is_cram = htsfile
-                .as_ref()
-                .expect("Problem accessing htsfile")
-                .is_cram()
-                != 0;
-            if is_cram {
-                match &cli.reference {
-                    Some(reference) => rhtslib_set_reference(htsfile, reference),
-                    None => {
-                        panic!("Alignment file is CRAM format but no reference file is specified!")
-                    }
-                };
-            }
-        }
+        let (htsfile, idx, header) = thread_setup(&cli.alignment, cli.reference.as_ref()).expect("Error during thread setup");
 
         for tr_region in tr_regions {
             let fetch_request = tr_region.reference_info.get_fetch_definition_s();
             if !cnv_regions.is_empty() {
                 if let Err(e) = tr_cn_from_cnvs(tr_region, &cnv_regions) {
-                    eprintln!("Thread {tidx}: Error setting copy number for region {fetch_request}: {e:?}");
+                    eprintln!("Thread {tidx}: Error setting copy number, skipping locus {fetch_request}: {e:?}");
                     continue
                 };
             }
 
-            let itr = rhtslib_fetch_by_str(idx, header, fetch_request.as_bytes());
+            let Ok(itr) = rhtslib_fetch_by_str(idx, header, fetch_request.as_bytes()) else {
+                eprintln!("Thread {tidx}: Error fetching reads, skipping locus {fetch_request}");
+                continue
+            };
+
             if let Err(e) = fetch_allele_lengths(tr_region, htsfile, itr, cli.flanksize) {
-                eprintln!("Thread {tidx}: Error fetching reads for region {fetch_request}: {e:?}");
+                eprintln!("Thread {tidx}: Error extracting allele lengths, skipping locus {fetch_request}: {e:?}");
                 // destroy iterator and continue to the next repeat region
                 unsafe {
                     htslib::hts_itr_destroy(itr);
@@ -168,7 +151,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             if let Err(e) =
                 estimate_genotype(tr_region, cli.reads_per_allele, Arc::clone(&partitions_map))
             {
-                eprintln!("Thread {tidx}: Could not estimate genotype for repeat {fetch_request}: {e:?}");
+                eprintln!("Thread {tidx}: Could not estimate genotype for locus {fetch_request}: {e:?}");
                 continue
             }
         }
@@ -180,7 +163,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    let (target_names, target_lengths) = get_target_names_lengths(cli.alignment.as_str())?;
+    let (target_names, target_lengths) = get_target_names_lengths(&cli.alignment)?;
     io_utils::trs_to_vcf(&tr_regions, &target_names, &target_lengths, &sample_name)?;
 
     Ok(())
@@ -189,7 +172,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn get_target_names_lengths(
     alignment_path: &str,
 ) -> Result<(Vec<String>, Vec<u64>), Box<dyn Error>> {
-    let htsfile = rhtslib_from_path(alignment_path);
+    let htsfile = rhtslib_from_path(alignment_path)?;
     let header: *mut htslib::sam_hdr_t = unsafe { htslib::sam_hdr_read(htsfile) };
     let hview = HeaderView::new(header);
     unsafe {
@@ -215,42 +198,70 @@ fn get_target_names_lengths(
     Ok((target_names, target_lengths))
 }
 
+fn thread_setup(
+    alignment_path: &str,
+    reference: Option<&String>,
+) -> Result<(*mut htsFile, *mut htslib::hts_idx_t, *mut htslib::sam_hdr_t)> {
+    let htsfile = rhtslib_from_path(alignment_path)?;
+    let header: *mut htslib::sam_hdr_t = unsafe { htslib::sam_hdr_read(htsfile) };
+    // we panic if we can't convert the alignment file name to a C string
+    let c_str = ffi::CString::new(alignment_path)
+        .context("Internal 0 byte contained in alignment file name")?;
+    let idx: *mut htslib::hts_idx_t = unsafe { htslib::sam_index_load(htsfile, c_str.as_ptr()) };
+    assert!(!idx.is_null(), "Unable to load index for alignment file!");
+
+    unsafe {
+        let is_cram = htsfile
+            .as_ref()
+            .with_context(|| "Problem acessing htsfile")?
+            .is_cram()
+            != 0;
+
+        if is_cram {
+            let reference = reference
+                .context("Alignment file is CRAM format but no reference file is specified!")?;
+            rhtslib_set_reference(htsfile, reference)?;
+        }
+    }
+
+    Ok((htsfile, idx, header))
+}
+
 // Functions below that are prefixed with 'rhtslib_' are private functions in
 // rust_htslib and are copied from there to be used in this binary.
 // It would be nicer to use rust_htslib's structs (e.g., IndexedReader) for reading
 // alignment files, but those structs cause segfaults when reading CRAM files (not BAM, interestingly)
 // when they were dropped, even on trivial tests. -> submit issue on GitHub?
-fn rhtslib_from_path<P: AsRef<Path>>(path: P) -> *mut htslib::htsFile {
-    let htsfile: *mut htslib::htsFile = rhtslib_hts_open(
-        &rust_htslib::utils::path_as_bytes(path, true).unwrap(),
-        b"r",
-    );
-    htsfile
+fn rhtslib_from_path<P: AsRef<Path>>(alignment_path: P) -> Result<*mut htslib::htsFile> {
+    let alignment_path = rust_htslib::utils::path_as_bytes(alignment_path, true)?;
+    let htsfile: *mut htslib::htsFile = rhtslib_hts_open(&alignment_path, b"r")?;
+    Ok(htsfile)
 }
 
-fn rhtslib_hts_open(path: &[u8], mode: &[u8]) -> *mut htslib::htsFile {
-    let cpath = ffi::CString::new(path).unwrap();
-    let c_str = ffi::CString::new(mode).unwrap();
+fn rhtslib_hts_open(path: &[u8], mode: &[u8]) -> Result<*mut htslib::htsFile> {
+    let cpath = ffi::CString::new(path)?;
+    let c_str = ffi::CString::new(mode)?;
     let ret = unsafe { htslib::hts_open(cpath.as_ptr(), c_str.as_ptr()) };
-    assert!(!ret.is_null(), "Unable to read alignment file!");
+    assert!(!ret.is_null(), "Unable to read alignment file");
 
-    ret
+    Ok(ret)
 }
 
-fn rhtslib_set_reference<P: AsRef<Path>>(htsfile: *mut htslib::htsFile, path: P) {
-    let res = unsafe { bam::set_fai_filename(htsfile, path) };
-    assert!(res.is_ok(), "Problem setting reference file!");
+fn rhtslib_set_reference<P: AsRef<Path>>(htsfile: *mut htslib::htsFile, path: P) -> Result<()> {
+    unsafe { bam::set_fai_filename(htsfile, path).context("Error setting reference file")? };
+    Ok(())
 }
 
 fn rhtslib_fetch_by_str(
     idx: *mut htslib::hts_idx_t,
     header: *mut htslib::sam_hdr_t,
     region: &[u8],
-) -> *mut htslib::hts_itr_t {
-    let rstr = ffi::CString::new(region).unwrap();
-    let rptr = rstr.as_ptr();
-    let itr = unsafe { htslib::sam_itr_querys(idx, header, rptr) };
-    assert!(!itr.is_null(), "Problem fetching reads from region!");
+) -> Result<*mut htslib::hts_itr_t> {
+    let rstr = ffi::CString::new(region)?;
+    let itr = unsafe { htslib::sam_itr_querys(idx, header, rstr.as_ptr()) };
+    if itr.is_null() {
+        bail!("Problem fetching reads from region")
+    };
 
-    itr
+    Ok(itr)
 }
